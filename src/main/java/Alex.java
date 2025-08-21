f76eeb650b6e71cb0710b15b5f449104d7b74ae7
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-08-21 21:30:59 +0800

    Full implemeted echo feature with better design

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -2,26 +2,33 @@ import java.util.Scanner;
 
 public class Alex {
 
+    // Method to generate a horizontal line
+    private static String lineGenerator() {
+        String s = "";
+        for (int i = 0; i < 100; i++) {
+            s = s + "_";
+        }
+        return s + "\n";
+    }
+
+    // field for a horizontal line
+    private static String line = Alex.lineGenerator();
+
     // Method to echo
     public static void echo() {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.next();
         if (s.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!\n");
+            System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
             scanner.close();
         } else {
-            System.out.println(s + "\n");
+            System.out.println(line + s + "\n" + line);
             echo();
         }
     }
     public static void main(String[] args) {
         String greeting = "Hello! I'm Alex.\n";
         String action = "What can I do for you?\n";
-        String line = "";
-        for (int i = 0; i < 100; i++) {
-            line = line + "_";
-        }
-        line = line + "\n";
         System.out.println(line + greeting + action + line);
         echo();
 
