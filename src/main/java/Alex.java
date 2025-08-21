d82580c8b8fea4de8e2581519fbc13ecf7a0d543
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-08-21 20:45:40 +0800

    Added a static method to handle responses

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,14 +1,21 @@
 public class Alex {
+
+    public static void echo(String s) {
+        if (s.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!\n");
+        } else {
+            System.out.println(s + "\n");
+        }
+    }
     public static void main(String[] args) {
         String greeting = "Hello! I'm Alex.\n";
         String action = "What can I do for you?\n";
-        String goodbye = "Bye. Hope to see you again soon!\n";
         String line = "";
         for (int i = 0; i < 100; i++) {
             line = line + "_";
         }
         line = line + "\n";
-        System.out.println(line + greeting + action + line + goodbye + line);
+        System.out.println(line + greeting + action + line);
 
     }
 }
