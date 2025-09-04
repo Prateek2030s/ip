712279e0e21c26c8ef7de3bed76e8659f36bce4b
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-09-04 22:34:01 +0800

    Add image for user and Alex

--- src/main/java/alexGUI/MainWindow.java
+++ src/main/java/alexGUI/MainWindow.java
@@ -23,8 +23,8 @@ public class MainWindow extends AnchorPane {
 
     private Alex alex;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Sanjai.jpg"));
+    private Image alexImage = new Image(this.getClass().getResourceAsStream("/images/Ale.jpg"));
 
     @FXML
     public void initialize() {
@@ -43,10 +43,10 @@ public class MainWindow extends AnchorPane {
     @FXML
     private void handleUserInput() {
         String input = userInput.getText();
-        String response = duke.getResponse(input);
+        String response = alex.getResponse(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, dukeImage)
+                DialogBox.getDukeDialog(response, alexImage)
         );
         userInput.clear();
     }
