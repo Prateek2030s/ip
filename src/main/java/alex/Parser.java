0ac1fa4165ef80eea29988fecbb0a0752d128b36
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-09-11 15:54:23 +0800

    Improve code quality of Storage.java

--- src/main/java/alex/Parser.java
+++ src/main/java/alex/Parser.java
@@ -31,133 +31,95 @@ public class Parser {
      * Parses user inputs and returns the chatbot's response.
      *
      * @return Chatbot's response to parsed user input.
-     * @throws AlexException if invalid inputs are passed into
+     * @throws AlexException If an invalid input is detected.
      */
     public String parseInput() throws AlexException {
 
         if (firstWord().equals("bye")) {
             return this.parseByeInput();
-
         } else if (firstWord().equals("list")) {
             return taskList.generateTaskList();
-
         } else if (firstWord().equals("mark")) {
-
-            if (inputBreakdown().length <= 1) {
-                throw new AlexException("Please state which task you would like to mark");
-            }
-
-            int taskNumber = Integer.parseInt(inputBreakdown()[1]);
-
-            if (taskNumber > taskList.size() || taskNumber < 0) {
-                throw new AlexException("Invalid number, please try again");
-            }
-
-             taskList.mark(taskNumber);
-
-
-            try {
-                storage.saveTask(taskList);
-            }
-            catch (IOException e) {
-                return ("File not found. Unable to save");
-            } finally {
-                return "Amazing, Task " + taskNumber + " marked.";
-            }
-
+            return this.parseMarkInput();
         } else if (firstWord().equals("unmark")) {
-
-            if (inputBreakdown().length <= 1) {
-                throw new AlexException("Please state which task you like to unmark");
-            }
-
-            int taskNumber = Integer.parseInt(inputBreakdown()[1]);
-
-            if (taskNumber > taskList.size() || taskNumber < 0) {
-                throw new AlexException("Invalid number, please try again");
-            }
-              taskList.unmark(taskNumber);
-
-            try {
-                storage.saveTask(taskList);
-            }
-            catch (IOException e) {
-                return ("File not found. Unable to save");
-            } finally {
-                return  "Amazing, Task " + taskNumber + " unmarked.";
-            }
-
+            return this.parseUnmarkInput();
         } else if (firstWord().equals("todo")) {
-
-            if (inputBreakdown().length <= 1) {
-                throw new AlexException("Please state what you would like todo");
-            }
-
-            Task toAdd = new Todo(inputBreakdown()[1]);
-            taskList.add(toAdd);
-
-            try {
-                storage.saveTask(taskList);
-            }
-            catch (IOException e) {
-                return ("File not found. Unable to save");
-            } finally {
-                String addTask = String.format("Ok, I've added this task: %s\n", toAdd);
-                String taskLength = "Watch out, you have " + taskList.size() + " tasks left.";
-                return addTask + taskLength;
-            }
-
+            return this.parseTodoInput();
         } else if (firstWord().equals("deadline")) {
-
-            if (inputBreakdown().length <= 1) {
-                throw new AlexException("Please state what deadline you have and by when");
-            }
-
-            String[] taskBreakdown = inputBreakdown()[1].split(" /by ");
-            String date = LocalDate.parse(taskBreakdown[1]).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-            Task toAdd = new Deadline(taskBreakdown[0], date);
-            taskList.add(toAdd);
-
-            try {
-                storage.saveTask(taskList);
-            }
-            catch (IOException e) {
-                return ("File not found. Unable to save");
-            } finally {
-                String addTask = String.format("Ok, I've added this task: %s\n", toAdd);
-                String taskLength = "Watch out, you have " + taskList.size() + " tasks left.";
-                return addTask + taskLength;
-            }
-
+            return this.parseDeadlineInput();
         } else if (firstWord().equals("event")) {
-
-            if (inputBreakdown().length <= 1) {
-                throw new AlexException("Please state when do you have the event");
-            }
-
-            String[] taskBreakdown = inputBreakdown()[1].split(" / ");
-            Task toAdd = new Event(taskBreakdown[0], taskBreakdown[1],taskBreakdown[2]);
-            taskList.add(toAdd);
-
-            try {
-                storage.saveTask(taskList);
-            }
-            catch (IOException e) {
-                return ("File not found. Unable to save");
-            } finally {
-                String addTask = String.format("Ok, I've added this task: %s\n", toAdd);
-                String taskLength = "Watch out, you have " + taskList.size() + " tasks left.";
-                return addTask + taskLength;
-            }
-
+            return this.parseEventInput();
         } else if (firstWord().equals("delete")) {
             return this.parseDeleteInput();
         } else if (firstWord().equals("find")) {
             return this.parseFindInput();
         } else if (firstWord().equals("hello")) {
-            return parseGreetingInput();
+            return this.parseGreetingInput();
         } else {
-            throw new AlexException("HAHAHA but I don't know what it means!");
+            return this.parseUnknownInput();
+        }
+    }
+
+    private String parseEventInput() throws AlexException {
+        if (inputBreakdown().length <= 1) {
+            throw new AlexException("Please state when do you have the event");
+        }
+
+        String[] taskBreakdown = inputBreakdown()[1].split(" / ");
+        Task toAdd = new Event(taskBreakdown[0], taskBreakdown[1],taskBreakdown[2]);
+        taskList.add(toAdd);
+
+        try {
+            storage.saveTask(taskList);
+        }
+        catch (IOException e) {
+            return ("File not found. Unable to save");
+        } finally {
+            String addTask = String.format("Ok, I've added this task: %s\n", toAdd);
+            String taskLength = "Watch out, you have " + taskList.size() + " tasks left.";
+            return addTask + taskLength;
+        }
+    }
+
+    private String parseDeadlineInput() throws AlexException {
+        if (inputBreakdown().length <= 1) {
+            throw new AlexException("Please state what deadline you have and by when");
+        }
+
+        String[] taskBreakdown = inputBreakdown()[1].split(" /by ");
+        String date = LocalDate.parse(taskBreakdown[1]).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        Task toAdd = new Deadline(taskBreakdown[0], date);
+        taskList.add(toAdd);
+
+        try {
+            storage.saveTask(taskList);
+        }
+        catch (IOException e) {
+            return ("File not found. Unable to save");
+        } finally {
+            String addTask = String.format("Ok, I've added this task: %s\n", toAdd);
+            String taskLength = "Watch out, you have " + taskList.size() + " tasks left.";
+            return addTask + taskLength;
+        }
+    }
+
+    private String parseTodoInput() throws AlexException {
+        if (inputBreakdown().length <= 1) {
+            throw new AlexException("Please state what you would like todo");
+        }
+
+        Task toAdd = new Todo(inputBreakdown()[1]);
+        taskList.add(toAdd);
+
+        try {
+            storage.saveTask(taskList);
+        }
+        catch (IOException e) {
+            return ("File not found. Unable to save");
+        } finally {
+            String addTask = String.format("Ok, I've added this task: %s\n", toAdd);
+            String taskLength = "Watch out, you have " + taskList.size() + " tasks left.";
+            return addTask + taskLength;
         }
     }
 
@@ -198,7 +160,54 @@ public class Parser {
         }
     }
 
+    public String parseMarkInput() throws AlexException {
+        if (inputBreakdown().length <= 1) {
+            throw new AlexException("Please state which task you would like to mark");
+        }
 
+        int taskNumber = Integer.parseInt(inputBreakdown()[1]);
 
+        if (taskNumber > taskList.size() || taskNumber < 0) {
+            throw new AlexException("Invalid number, please try again");
+        }
+
+        taskList.mark(taskNumber);
+
+        try {
+            storage.saveTask(taskList);
+        }
+        catch (IOException e) {
+            return ("File not found. Unable to save");
+        } finally {
+            return "Amazing, Task " + taskNumber + " marked.";
+        }
+    }
+    
+    public String parseUnmarkInput() throws AlexException {
+        
+        if (inputBreakdown().length <= 1) {
+            throw new AlexException("Please state which task you like to unmark");
+        }
+
+        int taskNumber = Integer.parseInt(inputBreakdown()[1]);
+
+        if (taskNumber > taskList.size() || taskNumber < 0) {
+            throw new AlexException("Invalid number, please try again");
+        }
+        taskList.unmark(taskNumber);
+
+        try {
+            storage.saveTask(taskList);
+        }
+        catch (IOException e) {
+            return ("File not found. Unable to save");
+        } finally {
+            return  "Amazing, Task " + taskNumber + " unmarked.";
+        }
+    }
+
+    public String parseUnknownInput() throws AlexException {
+        throw new AlexException("HAHAHA but I don't know what it means!");
+    }
 
 }
